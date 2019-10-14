0155423e75b8a3eb7d2c1ed17e05ca6822fd0014
Ryan Zeigler <zeiglerr@gmail.com>
2019-10-13 20:14:55 -0500

    A few more examples

--- src/main/scala/with_monads/parser/Parser.scala
+++ src/main/scala/with_monads/parser/Parser.scala
@@ -40,7 +40,18 @@ object Parser {
       _ <- advance(1)
     } yield c)
 
-  def string(str: String): Parser[String] = 
+  def charWhere(f: Char => Boolean): Parser[Char] =
+    Parser(for {
+      s <- getS
+      _ <- if (s.offset >= s.string.length)
+        fail(Error(s"expect a char", s.offset))
+      else Monad[M].unit
+      c <- if (f(s.string.charAt(s.offset))) Monad[M].pure(s.string.charAt(s.offset))
+      else fail(Error(s"expected a char", s.offset))
+      _ <- advance(1)
+    } yield c)
+
+  def string(str: String): Parser[String] =
     Parser(for {
       s <- getS
       _ <- if (s.string.slice(s.offset, str.length) != str)
@@ -49,27 +60,49 @@ object Parser {
       _ <- advance(str.length)
     } yield str)
 
-  def regex(re: Regex): Parser[String] = 
+  def regex(re: Regex): Parser[String] =
     Parser(for {
       s <- getS
-      m <- re.findPrefixMatchOf(s.string.drop(s.offset)).fold(fail[String](Error(s"expected $re", s.offset)))(m => Monad[M].pure(m.matched))
+      m <- re
+        .findPrefixMatchOf(s.string.drop(s.offset))
+        .fold(fail[String](Error(s"expected $re", s.offset)))(
+          m => Monad[M].pure(m.matched)
+        )
       _ <- advance(m.length)
     } yield m)
 
-  def opt[A](p: Parser[A]): Parser[Option[A]] = 
-    Parser(backtrack(p.parser.map[Option[A]](Some(_)))
-      .recoverWith(_ => Monad[M].pure(None)))
+  def opt[A](p: Parser[A]): Parser[Option[A]] =
+    Parser(
+      backtrack(p.parser.map[Option[A]](Some(_)))
+        .recoverWith(_ => Monad[M].pure(None))
+    )
 
-  def int: Parser[Int] = 
+  def many[A](p: Parser[A]): Parser[List[A]] = {
+    def manyImpl(from: List[A]): Parser[List[A]] =
+      opt(p).flatMap(_.fold(Monad[Parser].pure(from))(a => manyImpl(a :: from)))
+
+    always(List.empty[A]).flatMap(manyImpl)
+  }
+
+  def many1[A](p: Parser[A]): Parser[List[A]] =
+    p.flatMap(head => many(p).map(head :: _))
+
+
+  def digit: Parser[Char] = 
+    charWhere(Set('0', '1', '2', '3', '4', '5', '6', '7', '8', '9').contains)
+
+
+  def int: Parser[Int] =
     for {
       sign <- opt(char('-'))
-      text <- regex("0|([1-9][0-9]*)".r)
-    } yield ???
+      text <- many1(digit)
+    } yield (sign.fold("")(_.toString) ++ text).toInt
 
-  lazy val eof: Parser[Unit] = 
+  lazy val eof: Parser[Unit] =
     Parser(for {
       s <- getS
-      _ <- if (s.offset == s.string.length) Monad[M].unit else fail(Error("expected eof", s.offset))
+      _ <- if (s.offset == s.string.length) Monad[M].unit
+      else fail(Error("expected eof", s.offset))
     } yield ())
 
   // Helper algebras for working with the complicated monad
@@ -82,7 +115,7 @@ object Parser {
 
   private def fail[A](e: Error): M[A] = EitherT.left[Inner, Error, A](e)
 
-  private def backtrack[A](ma: M[A]): M[A] = 
+  private def backtrack[A](ma: M[A]): M[A] =
     getS.flatMap(s => ma.recoverWith(e => setS(s) >> EitherT.left(e)))
 
   private def modify(f: S => S): M[S] =
@@ -92,7 +125,8 @@ object Parser {
     modify(s => s.copy(offset = s.offset + ct)).void
 
   implicit val monadForParser: Monad[Parser] = new Monad[Parser] {
-    def flatMap[A, B](fa: Parser[A])(g: A => Parser[B]): Parser[B] = fa.flatMap(g)
+    def flatMap[A, B](fa: Parser[A])(g: A => Parser[B]): Parser[B] =
+      fa.flatMap(g)
     def pure[A](a: A): Parser[A] = Parser(Monad[M].pure(a))
   }
 }

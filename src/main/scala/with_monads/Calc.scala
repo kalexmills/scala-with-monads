da4a9f8abb78d479d19e4cd80ce337f24c9c94ef
Ryan Zeigler <ryan.zeigler@s5stratos.com>
2019-10-14 05:57:22 -0500

    Fix the calculator

--- src/main/scala/with_monads/Calc.scala
+++ src/main/scala/with_monads/Calc.scala
@@ -7,22 +7,22 @@ object Calc {
 
   def paren[A](p: Parser[A]): Parser[A] = char('(') >> p flatTap(_ => char(')'))
 
-  lazy val high = {
+  lazy val high: Parser[Int] = {
     val single = atom
     val expr = for {
     left <- atom
     op <- char('*') or char('/')
-    right <- atom
+    right <- high
   } yield if (op == '*') left * right else left / right
   expr or single
 }
 
-  lazy val low = {
+  lazy val low: Parser[Int] = {
     val single = high
     val expr = for {
       left <- high
       op <- char('+') or char('-')
-      right <- high
+      right <- low
     } yield if (op == '+') left + right else left - right
 
     expr or single

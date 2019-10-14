1173de2998172ac873937cab7cd5e82977d33d74
Corey McKenzie <Corey.McKenzie@S5Stratos.com>
2019-10-14 15:56:12 -0500

    Fixes to make it work for generic.

--- src/test/scala/with_monads.scala/StackSpec.scala
+++ src/test/scala/with_monads.scala/StackSpec.scala
@@ -23,16 +23,16 @@ class StackSpec extends FlatSpec {
   }
 
   def popPopPush[A](implicit ev: Monoid[A]): State[List[A], A] = for {
-    x <- pop.map(_.getOrElse(Monoid.empty))
-    y <- pop.map(_.getOrElse(Monoid.empty))
-    _ <- push(Monoid.combine(x, y))
-  } yield (Monoid.combine(x, y))
+    x <- pop[A].map(_.getOrElse(ev.empty))
+    y <- pop[A].map(_.getOrElse(ev.empty))
+    _ <- push(ev.combine(x, y))
+  } yield (ev.combine(x, y))
   
   "Stack" should "be (List(3, 3), 3) for [x = pop(), y = pop(), push(x+y)] on List(1, 2, 3)" in {
-    popPopPush.run(List(1, 2, 3)) should equal(List(3, 3), 3)
+    popPopPush[Int].run(List(1, 2, 3)) should equal(List(3, 3), 3)
   }
 
   it should "be (List(0), 0) for [x = pop(), y = pop(), push(x+y)] on List.empty" in {
-    popPopPush.run(List.empty) should equal(List(0), 0)
+    popPopPush[Int].run(List.empty) should equal(List(0), 0)
   }
 }
